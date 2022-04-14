16ef448d68c5c8ebffa62b031e2448070bb88200
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-13 20:20:07 -0400

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -3,3 +3,26 @@ fetch("https://opentdb.com/api.php?amount=10&category=18")
 .then(response => response.json())
 .then(data => console.log(data));
 
+// ---------------------------------------------------------------------
+//getting the button to work and start timer
+
+let clockId;
+let time = 60;
+let banner = document.querySelector('.banner');
+let clock = document.querySelector('#time');
+document.getElementById('start').addEventListener('click',handleStart);
+
+function handleStart() {
+    clockId = setInterval(handleClock,1000);
+    banner.innerHTML = `<div><h1>${questions[1].question}</h1></div> `
+};
+
+function handleClock() {
+    clock.innerText = time;
+    if (time<1) {
+        clock.innerText = 0;
+        return clearInterval(clockId);
+    };
+    time--;
+};
+// ---------------------------------------------------------------------
